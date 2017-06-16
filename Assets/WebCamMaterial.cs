8cd4153dadd0efb5ac96c39a7912fcc4b6765ec7
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2017-06-17 00:14:13 +0900

    initial commit

---
+++ Assets/WebCamMaterial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamMaterial : MonoBehaviour {
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        // display all cameras
+        for (var i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+        }
+        WebCamTexture webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);
+        GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+SphereCamPlayer
+===============
+
+## Import assets
+
+* SteamVR
+* Sphere100.fbx
+https://twitter.com/warapuri/status/480915916645863424
+
+
+## Modified params
+
+### phong1 of Sphere100
+
+* `Tiling X` : 1 to -1
+* `Offset X` : 0 to  1
