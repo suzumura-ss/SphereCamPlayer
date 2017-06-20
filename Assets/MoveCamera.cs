3df74b3db4869fe31cb914c636c2a643a533dcee
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2017-06-20 19:54:50 +0900

    カーソルキーでのカメラ移動を実装 / HMDとの同時不可

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    // Use this for initialization
+    private float _yaw = 0;
+    private float _pitch = 0;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private static Vector3 _Yaxis = new Vector3(0, 1, 0);
+    private static Vector3 _Xaxis = new Vector3(1, 0, 0);
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _yaw += 1.0f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _yaw -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && _pitch > -90.0f)
+        {
+            _pitch -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && _pitch < 90.0f)
+        {
+            _pitch += 1.0f;
+        }
+        transform.LookAt(new Vector3(0, 0, 1));
+        transform.Rotate(_Yaxis, _yaw);
+        transform.Rotate(_Xaxis, _pitch);
+    }
+}
--- Assets/WebCamMaterial.cs
+++ Assets/WebCamMaterial.cs
@@ -10,12 +10,18 @@ public class WebCamMaterial : MonoBehaviour {
     void Start()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
+        var list = new List<WebCamDevice>(devices);
+        int uc_index = list.FindIndex((WebCamDevice dev) => { return dev.name.EndsWith("UVC Capture"); });
+        if (uc_index < 0)
+        {
+            uc_index = 0;
+        }
         // display all cameras
         for (var i = 0; i < devices.Length; i++)
         {
             Debug.Log(devices[i].name);
         }
-        WebCamTexture webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);
+        WebCamTexture webcamTexture = new WebCamTexture(devices[uc_index].name, Width, Height, FPS);
         GetComponent<Renderer>().material.mainTexture = webcamTexture;
         webcamTexture.Play();
     }
